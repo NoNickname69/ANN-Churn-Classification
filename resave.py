1624e6a41643d70fd45d64c93153df9d347d9a26
Dayyan <gogglesrealitygoggles@gmail.com>
2026-06-04 03:59:26 +0530

    Removed files which were not needed anymore [runtime, .python-version, resave.py]

--- resave.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf 
-import keras 
-model = keras.models.load_model('C:/Users/goggl/ANN Example/model.keras') 
-model.save('C:/Users/goggl/ANN Example/model_v2.h5') 
-print('Done!') 
